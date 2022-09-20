e688bc723d3c4a218a43092231a452fe86023158
GitHub Action <action@github.com>
2022-09-20 08:20:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/leavestylecoder/functionalprogramminganalysis/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/leavestylecoder/functionalprogramminganalysis/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.leavestylecoder.functionalprogramminganalysis.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leavestylecoder.functionalprogramminganalysis.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/leavestylecoder/functionalprogramminganalysis/services/MyProjectService.kt
+++ src/main/kotlin/com/github/leavestylecoder/functionalprogramminganalysis/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.leavestylecoder.functionalprogramminganalysis.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leavestylecoder.functionalprogramminganalysis.MyBundle
 
 class MyProjectService(project: Project) {
 
